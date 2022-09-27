3131e62925c76e51773f8a45078ec7c79d4db32b
大大东 <lidong@021.com>
2022-09-27 17:18:32 +0800

    增加注释

--- DLConfuse/HardStringUnit/HardStringEncryptDecryptUnit.h
+++ DLConfuse/HardStringUnit/HardStringEncryptDecryptUnit.h
@@ -10,17 +10,22 @@
 
 
 /// 加密 (工具使用此方法生成密文)
-static inline NSString *_Nonnull XYZ_encriptHardString(NSString *hstring) {
+/// 加密算法
+/// 1. 生成一个[3, 8]的随机数a
+/// 2. 把明文进行base64编码得到b
+/// 3. 如果b的长度大于a, 则对b进行倒序插入随机字符, 插入间隔为a, 否则不处理
+/// 4. 把a拼接在b的第一个字符
+static inline NSString *_Nonnull XYZ_encriptHardString(NSString * _Nonnull hstring) {
     
     NSString *base64 = [[hstring dataUsingEncoding:NSUTF8StringEncoding] base64EncodedStringWithOptions:0];
-    int intervel = arc4random() % 6 + 3; // 3 - 9
+    int intervel = arc4random() % 6 + 3; // 3 - 8
     
     NSMutableString *newstring = [base64 mutableCopy];;
     if (intervel < base64.length) {
         NSArray *arr = @[@"a", @"1", @"b", @"2", @"c", @"3", @"d", @"4", @"5", @"e", @"6", @"f", @"7", @"8", @"9"];
         
         int count = 0;
-        for (NSInteger i = base64.length - 1; i > 0 ; i--) {
+        for (NSInteger i = base64.length - 1; i >= 0 ; i--) {
             count++;
             if (count == intervel) {
                 [newstring insertString:arr[arc4random() % arr.count] atIndex:i];
@@ -35,7 +40,7 @@ static inline NSString *_Nonnull XYZ_encriptHardString(NSString *hstring) {
 
 
 /// 解密 (工具把原明文字符串 替换为XYZ_decriptHardString(密文))
-static inline NSString *_Nonnull XYZ_decriptHardString(NSString *hstring) {
+static inline NSString *_Nonnull XYZ_decriptHardString(NSString * _Nonnull hstring) {
     
     // 取出插入间隔
     int intervel = [[hstring substringToIndex:1] intValue];
@@ -46,7 +51,7 @@ static inline NSString *_Nonnull XYZ_decriptHardString(NSString *hstring) {
     if (intervel <= newString.length) {
         mNewtring = [newString mutableCopy];
         int count = 0;
-        for (NSInteger i = newString.length - 1; i > 0 ; i--) {
+        for (NSInteger i = newString.length - 1; i >= 0 ; i--) {
             count++;
             if (count == intervel + 1) { // 这里需要加1
                 [mNewtring deleteCharactersInRange:NSMakeRange(i, 1)];
